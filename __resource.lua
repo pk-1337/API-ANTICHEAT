c453e8d2f6ab4fa56d45b3f334b9b23dc6e83b4b
PK <59345284+pk-1337@users.noreply.github.com>
2020-12-03 13:50:31 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+description 'API-ANTICHEAT 7.0A discord.gg/apiac'
+
+server_scripts{
+	'configs/serverconfig.lua',
+	'configs/config.lua',
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'installer.lua',
+	'server.lua'
+} 
+
+client_scripts {
+	'configs/config.lua',
+	'client.lua'
+}
+
